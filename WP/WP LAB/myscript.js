258c920abced6c8646c4d44986915acf9c713d8f
csandeepreddy <sandeepreddy.c6543@gmail.com>
2020-12-11 19:22:57 +0530

    script

---
+++ WP/WP LAB/myscript.js
@@ -0,0 +1,10 @@
+
+function myfunction(){
+  document.getElementById("about").innerHTML="sandeep reddy";
+  document.getElementById("codeblocks").innerHTML="Hello baby";
+  document.getElementById("codeblocks").innerHTML="h r u!";
+
+}
+function myFunction(p1, p2) {
+  return p1 * p2;
+}
