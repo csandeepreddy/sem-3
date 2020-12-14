6e20dd67f30340d2ba1a8cccc1ebeab188573717
csandeepreddy <sandeepreddy.c6543@gmail.com>
2020-12-15 04:53:12 +0530

    script

--- WP/WP LAB/myscript.js
+++ WP/WP LAB/myscript.js
@@ -8,3 +8,22 @@ function myfunction(){
 function myFunction(p1, p2) {
   return p1 * p2;
 }
+function myFunction() {
+  var message, x;
+  message = document.getElementById("p01");
+  message.innerHTML = "";
+  x = document.getElementById("errors").value;
+  try {
+    if(x == "")  throw "is empty";
+    if(isNaN(x)) throw "is not a number";
+    x = Number(x);
+    if(x > 10)   throw "is too high";
+    if(x < 5)  throw "is too low";
+  }
+  catch(err) {
+    message.innerHTML = "Input " + err;
+  }
+  finally {
+    document.getElementById("errors").value = "";
+  }
+}
