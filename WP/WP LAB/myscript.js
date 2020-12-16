9ab85365c6f9523d9bcce584acf50c0e858f261e
csandeepreddy <sandeepreddy.c6543@gmail.com>
2020-12-16 19:41:52 +0530

    add

--- WP/WP LAB/myscript.js
+++ WP/WP LAB/myscript.js
@@ -1,3 +1,17 @@
+function myFunction() {
+  var x, text;
+
+  // Get the value of the input field with id="numb"
+  x = document.getElementById("numb").value;
+
+  // If x is Not a Number or less than one or greater than 10
+  if (isNaN(x) || x < 1 || x > 10) {
+    text = "Input not valid";
+  } else {
+    text = "Input OK";
+  }
+  document.getElementById("form").innerHTML = text;
+}
 
 function myfunction(){
   document.getElementById("about").innerHTML="sandeep reddy";
